5480b9662e9722bfbfc05abbaee044c19939a4d6
Chris <christoph.sonnleitner@outlook.com>
2023-05-11 22:11:14 +0200

    add functions

--- lua/telescope/_extensions/my_finders.lua
+++ lua/telescope/_extensions/my_finders.lua
@@ -1,4 +1,3 @@
-
 local telescope = require('telescope')
 local pickers = require('telescope.pickers')
 local make_entry = require('telescope.make_entry')
@@ -17,9 +16,31 @@ local flatten = vim.tbl_flatten
 local filter = vim.tbl_filter
 local lib = require('telescope._extensions.lib')
 
+
+local get_git_root = function()
+    local handle = io.popen("(git rev-parse --show-toplevel) 2> /dev/null || pwd")
+    local result = handle:read("*a")
+    handle:close()
+    local format = result:gsub('[\n\r]', '')
+    return format
+end
+
+
+local find_files = function(opts)
+    opts = opts or {}
+    git_root = get_git_root_alt()
+    opts.entry_maker = make_entry.gen_from_file(opts)
+    pickers.new(opts, {
+        prompt_title = get_git_root(),
+        finder = finders.new_oneshot_job({ "ag", "-l", "-f", "-p", "~/.agignore", "--hidden", "", git_root }, opts),
+        previewer = conf.file_previewer(opts),
+        sorter = conf.file_sorter(opts),
+    }):find()
+end
+
 local my_finders = function(opts)
-    if opts.type == "first" then
-        echo "first"
+    if opts.type == "find_files" then
+        find_files(opts)
     elseif opts.type == "second" then
         echo "second"
     else
